6f87ef9caae11aa9200caaa6c6f4d86e4cf32bcf
Lilyppu <lily@ppu.co.id>
2024-09-17 10:57:18 +0700

    update neraca by Lily

--- src/main/java/com/GeneralLedger/Services/ServiceTNeracaMonthly.java
+++ src/main/java/com/GeneralLedger/Services/ServiceTNeracaMonthly.java
@@ -6,9 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.GeneralLedger.Models.GlPjDetail;
 import com.GeneralLedger.Models.TNeracaMonthly;
-import com.GeneralLedger.Repository.IGlPjDetailRepository;
 import com.GeneralLedger.Repository.ITNeracaMonthlyRepository;
 
 @Service
@@ -20,4 +18,21 @@ public class ServiceTNeracaMonthly {
 		repoTnm.runNeraca(pmonth, pmsg);
 		return repoTnm.findAll();
 	}
+	
+	public String getCoyDesc(String pMon) {
+		return repoTnm.getCoyDesc(pMon);
+	}
+	
+	public String getMonDesc(String pMon) {
+		return repoTnm.getMonDesc(pMon);
+	}
+	
+	public List getNeracaPList(String pMon) {
+		return repoTnm.getNeracaPList(pMon);
+	}
+	
+	public Integer getLevelMax(String pMon){
+		return repoTnm.getLevelMax(pMon);
+	}
+
 }
